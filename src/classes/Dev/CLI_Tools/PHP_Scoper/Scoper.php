c8e6db2659c73232dd93e800ae5e311f746d15f9
Mike Taykle <miketaykle@o5p.me>
2022-01-16 23:19:59 -0500

    update

--- src/classes/Dev/CLI_Tools/PHP_Scoper/Scoper.php
+++ src/classes/Dev/CLI_Tools/PHP_Scoper/Scoper.php
@@ -205,7 +205,7 @@ final class Scoper extends U\A6t\CLI_Tool {
 			// {@see https://github.com/squizlabs/PHP_CodeSniffer/issues/1818#issuecomment-354420927}.
 			3 <= U\CLI::run( [
 				[ $this->project->dir . '/vendor/bin/phpcbf' ],
-				[ '-pvv', '--parallel=4', '--standard=' . $standard ],
+				[ '-pv', '--parallel=4', '--standard=' . $standard ],
 				[ '--extensions=php', '--ignore=' . $ignore ],
 				$output_dir, // ← directory to fix.
 			], $this->project->dir, false ) ) {
