7b32bd907f16f1acfd16f4408aecb420e052d918
Mike Taykle <miketaykle@o5p.me>
2022-03-05 04:47:41 -0500

    update

--- dev/libraries/dotfiles/dev/.libs/docker/wp/entry.bash
+++ dev/libraries/dotfiles/dev/.libs/docker/wp/entry.bash
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 ##
+# Clever Canyon™ {@see https://clevercanyon.com}
+#
+#  CCCCC  LL      EEEEEEE VV     VV EEEEEEE RRRRRR      CCCCC    AAA   NN   NN YY   YY  OOOOO  NN   NN ™
+# CC      LL      EE      VV     VV EE      RR   RR    CC       AAAAA  NNN  NN YY   YY OO   OO NNN  NN
+# CC      LL      EEEEE    VV   VV  EEEEE   RRRRRR     CC      AA   AA NN N NN  YYYYY  OO   OO NN N NN
+# CC      LL      EE        VV VV   EE      RR  RR     CC      AAAAAAA NN  NNN   YYY   OO   OO NN  NNN
+#  CCCCC  LLLLLLL EEEEEEE    VVV    EEEEEEE RR   RR     CCCCC  AA   AA NN   NN   YYY    OOOO0  NN   NN
+##
+
+##
 # WP docker entrypoint.
 #
 # @since 1.0.0
@@ -10,6 +20,7 @@
 # - Instead of editing this file, you can modify `./dev/.libs/docker/wp/entry~prj.bash`.
 # - Instead of editing this file, please review source repository {@see https://o5p.me/LevQOD}.
 ##
+
 # ---------------------------------------------------------------------------------------------------------------------
 # Source a few dependencies.
 # ---------------------------------------------------------------------------------------------------------------------
