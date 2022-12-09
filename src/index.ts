acbb20d7d34869d3a05133142fcbd71a517241aa
Jason Caldwell <jaswrks@o5p.me>
2022-12-09 15:03:32 -0500

    Update.

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,9 @@
-import { default as Brand }  from './Brand';
-import { default as Cookie } from './Cookie';
-import { default as Crypto } from './Crypto';
-import { default as Env }    from './Env';
-import { default as HTTP }   from './HTTP';
-import { default as Obj }    from './Obj';
-import { default as Str }    from './Str';
-import { default as Time }   from './Time';
-import { default as URL }    from './URL';
-
-export default {
-	Brand,
-	Cookie,
-	Crypto,
-	Env,
-	HTTP,
-	Obj,
-	Str,
-	Time,
-	URL,
-};
+export { default as Brand }  from './Brand';
+export { default as Cookie } from './Cookie';
+export { default as Crypto } from './Crypto';
+export { default as Env }    from './Env';
+export { default as HTTP }   from './HTTP';
+export { default as Obj }    from './Obj';
+export { default as Str }    from './Str';
+export { default as Time }   from './Time';
+export { default as URL }    from './URL';
