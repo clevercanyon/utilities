f177dc70bf7c4e249b3210d0c4d6d6ff04052c96
Jason Caldwell <jaswrks@o5p.me>
2022-12-13 17:05:21 -0500

    Update.

---
+++ src/Git.ts
@@ -0,0 +1,63 @@
+/**
+ * Utility class.
+ */
+
+/**
+ * Git utilities.
+ */
+export default class $Git {
+	/**
+	 * Converts a `.gitignore` file into glob patterns.
+	 *
+	 * @param   ignoreFileContents `.gitignore` file contents.
+	 * @param   options            An options object (which is optional).
+	 *
+	 * @returns                    An array of glob patterns compatible with `micromatch` and `fast-glob`.
+	 *
+	 * @see https://git-scm.com/docs/gitignore
+	 */
+	public static ignoreGlobs(ignoreFileContents: string, options = { negate: true }): Array<string> {
+		const globPatterns = [];
+
+		for (let line of ignoreFileContents.trim().split(/[\r\n]+/u)) {
+			line = line.trim();
+
+			if (0 === line.indexOf('#')) {
+				continue; // Comment line.
+			} else if ('' === line || '!' === line) {
+				continue; // Actually an empty line.
+			}
+			let glob = line; // Glob pattern.
+
+			const isNegated = 0 === glob.indexOf('!');
+			glob = isNegated ? glob.slice(1) : glob;
+
+			const slashIndex = glob.indexOf('/');
+			const containsSlash = -1 !== slashIndex;
+			const startsWithSlash = 0 === slashIndex;
+			const endsWithSlash = slashIndex === glob.length - 1;
+			const isRelative = startsWithSlash || (containsSlash && slashIndex < glob.length - 1);
+
+			if (startsWithSlash) {
+				glob = glob.slice(1);
+			}
+			if (endsWithSlash) {
+				glob = glob.slice(0, -1);
+			}
+			if (!isRelative && !glob.startsWith('**/')) {
+				glob = '**/' + glob;
+			}
+			if (!glob.endsWith('/**')) {
+				glob = glob + '/**';
+			}
+			if (isNegated) {
+				glob = '!' + glob;
+			}
+			if (options.negate) {
+				glob = 0 === glob.indexOf('!') ? glob.slice(1) : '!' + glob;
+			}
+			globPatterns.push(glob);
+		}
+		return globPatterns;
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export { default as $Brand } from './Brand';
 export { default as $Cookie } from './Cookie';
 export { default as $Crypto } from './Crypto';
 export { default as $Env } from './Env';
+export { default as $Git } from './Git';
 export { default as $HTTP } from './HTTP';
 export { default as $Obj } from './Obj';
 export { default as $Str } from './Str';
