1b88805563e61c25b6dbd33ba93e71d5e27f89b8
Jason Caldwell <jaswrks@o5p.me>
2022-12-16 02:29:29 -0500

    Update.

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@
  * Utility exports.
  */
 
-export * as brand from './brand';
-export * as cookie from './cookie';
-export * as crypto from './crypto';
-export * as env from './env';
-export * as http from './http';
-export * as obj from './obj';
-export * as str from './str';
-export * as time from './time';
-export * as url from './url';
+export * as $brand from './brand';
+export * as $cookie from './cookie';
+export * as $crypto from './crypto';
+export * as $env from './env';
+export * as $http from './http';
+export * as $obj from './obj';
+export * as $str from './str';
+export * as $time from './time';
+export * as $url from './url';
