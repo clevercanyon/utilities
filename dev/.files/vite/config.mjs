884be0dffa48909c6159172595e67bb753a4d5b5
Jason Caldwell <jaswrks@o5p.me>
2023-03-21 00:30:51 -0400

    Project update. [p][robotic]

--- dev/.files/vite/config.mjs
+++ dev/.files/vite/config.mjs
@@ -125,11 +125,8 @@ export default async ({ mode, command /*, ssrBuild */ }) => {
 
 	updatePkg.type = 'module'; // ES module; always.
 	updatePkg.exports = {}; // Exports object initialization.
-	updatePkg.sideEffects = ['./src/*.{html,scss,ts,tsx}']; // <https://o5p.me/xVY39g>.
+	updatePkg.sideEffects = []; // <https://o5p.me/xVY39g>.
 
-	if (fs.existsSync(path.resolve(projDir, './src/resources/init-env.ts'))) {
-		updatePkg.sideEffects.push('./src/resources/init-env.ts');
-	}
 	if (isCMA && (isSSR || cmaEntries.length > 1)) {
 		updatePkg.exports = {
 			'.': {
@@ -159,6 +156,8 @@ export default async ({ mode, command /*, ssrBuild */ }) => {
 				},
 			});
 		}
+		updatePkg.sideEffects = ['./src/*.{html,scss,ts,tsx}'];
+		//
 	} else if (isCMA) {
 		updatePkg.exports = {
 			'.': {
@@ -175,10 +174,16 @@ export default async ({ mode, command /*, ssrBuild */ }) => {
 
 		updatePkg.types = './dist/types/' + cmaEntryIndexSubpathNoExt + '.d.ts';
 		updatePkg.typesVersions = { '>=3.1': { './*': ['./dist/types/*'] } };
-	} else {
+
+		updatePkg.sideEffects = ['./src/*.{html,scss,ts,tsx}'];
+		//
+	} /* Multipage app. */ else {
 		updatePkg.type = 'module'; // Always a module when building with Vite.
 		updatePkg.module = updatePkg.main = updatePkg.browser = updatePkg.unpkg = updatePkg.types = '';
 		(updatePkg.exports = null), (updatePkg.sideEffects = []), (updatePkg.typesVersions = {});
+	}
+	if (fs.existsSync(path.resolve(projDir, './src/resources/init-env.ts'))) {
+		updatePkg.sideEffects.push('./src/resources/init-env.ts');
 	}
 
 	/**
@@ -267,8 +272,8 @@ export default async ({ mode, command /*, ssrBuild */ }) => {
 	/**
 	 * Configures rollup for Vite.
 	 *
-	 * @see https://vitejs.dev/config/build-options.html#build-rollupoptions
 	 * @see https://rollupjs.org/guide/en/#big-list-of-options
+	 * @see https://vitejs.dev/config/build-options.html#build-rollupoptions
 	 */
 	const rollupConfig = {
 		input: isCMA // Absolute paths.
@@ -293,10 +298,10 @@ export default async ({ mode, command /*, ssrBuild */ }) => {
 			// recommended, because preserving module structure in a final build has performance costs.
 			// However, in builds that are not final (e.g., CMAs with peer dependencies), preserving modules
 			// has performance benefits, as it allows for tree-shaking optimization in final builds.
-			preserveModules: isCMA && Object.keys(pkg.peerDependencies || {}).length > 0,
+			preserveModules: isCMA && cmaEntries.length > 1 && Object.keys(pkg.peerDependencies || {}).length > 0,
 
 			// Cannot inline dynamic imports when `preserveModules` is enabled, so set as `false` explicitly.
-			...(isCMA && Object.keys(pkg.peerDependencies || {}).length > 0 ? { inlineDynamicImports: false } : {}),
+			...(isCMA && cmaEntries.length > 1 && Object.keys(pkg.peerDependencies || {}).length > 0 ? { inlineDynamicImports: false } : {}),
 		},
 	};
 	// <https://vitejs.dev/guide/features.html#web-workers>
@@ -413,6 +418,7 @@ export default async ({ mode, command /*, ssrBuild */ }) => {
 	 */
 	const baseConfig = {
 		c10n: { pkg, updatePkg },
+
 		define: /* Static replacements. */ {
 			$$__APP_PKG_NAME__$$: JSON.stringify(pkg.name || ''),
 			$$__APP_PKG_VERSION__$$: JSON.stringify(pkg.version || ''),
