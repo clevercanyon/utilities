401fb9e2d8e0e95aa70f5de376a7735555ebb764
Jason Caldwell <jaswrks@o5p.me>
2023-03-21 08:10:54 -0400

    Project update. [p][robotic]

--- src/react.tsx
+++ src/react.tsx
@@ -17,9 +17,9 @@ import { castArray as $toꓺcastArray } from './to.js';
 /**
  * Defines types.
  */
-export interface Props {
+export type Props = {
 	readonly classes?: string | string[];
-}
+};
 export type Component<P extends Props = Props> = React.FunctionComponent<P>;
 export type ClassComponent<P extends Props = Props> = React.ComponentClass<P>;
 export type AnyComponent<P extends Props = Props> = Component<P> | ClassComponent<P>;
