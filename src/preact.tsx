6c02a0623d3726cc1e339ecf73532ebf1a6aedfb
Jason Caldwell <jaswrks@o5p.me>
2023-08-16 07:55:45 -0400

    Project update. [p][robotic]

--- src/preact.tsx
+++ src/preact.tsx
@@ -2,8 +2,21 @@
  * Preact utilities.
  */
 
+import {
+	Router, //
+	Route,
+	Link,
+	Switch,
+	Redirect,
+	useRoute,
+	useLocation,
+	useRouter,
+} from 'wouter-preact';
+
 import preact from 'preact';
 import { castArray as $toꓺcastArray } from './to.js';
+import { defaults as $objꓺdefaults } from './obj.js';
+import { render as preactꓺrenderToString } from 'preact-render-to-string';
 
 /**
  * Defines types.
@@ -14,6 +27,18 @@ export type Props<P extends object = object> = preact.RenderableProps<P & { clas
 
 export type Context = object; // Nothing to inherit from preact types.
 export type State = object; // Nothing to inherit from preact types.
+
+export type RenderToStringOptions = { context?: Context };
+
+/**
+ * Exports Wouter components.
+ */
+export { Router, Route, Link, Switch, Redirect };
+
+/**
+ * Exports Wouter APIs.
+ */
+export { useRoute, useLocation, useRouter };
 
 /**
  * Formats component classes.
@@ -30,4 +55,17 @@ export const classes = (classes: string | string[], props: Props): string => {
 	propClasses = $toꓺcastArray('' === propClasses ? [] : propClasses);
 
 	return [...new Set(classes.concat(propClasses))].join(' ');
+};
+
+/**
+ * Renders vNode tree as a string.
+ *
+ * @param   vNode   VNode to render as a string.
+ * @param   options Options (all optional); {@see RenderToStringOptions}.
+ *
+ * @returns         VNode tree rendered as a string.
+ */
+export const renderToString = (vNode: VNode, options?: RenderToStringOptions): string => {
+	const opts = $objꓺdefaults({}, options || {}, { context: {} }) as Required<RenderToStringOptions>;
+	return preactꓺrenderToString(vNode, opts.context);
 };
--- src/preact/apis.ts
+++
@@ -1,6 +0,0 @@
-/**
- * All APIs.
- */
-
-export * as $ssr from './apis/ssr/index.js';
-export * as $router from './apis/router/index.js';
