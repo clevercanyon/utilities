f26145c963ea4b572ca1db017106b20a8c716575
Jason Caldwell <jaswrks@o5p.me>
2023-08-19 05:11:09 -0400

    Project update. [p][robotic]

--- src/preact/components/router/index.tsx
+++ src/preact/components/router/index.tsx
@@ -3,7 +3,8 @@
  */
 
 import * as $preact from '../../../preact.js';
-import { Location, ErrorBoundary, Router } from '../../apis/iso.js';
+import { empty as $isꓺempty } from '../../../is.js';
+import { Location, ErrorBoundary, Router, useLocation } from '../../apis/iso.js';
 
 /**
  * Props interface.
@@ -19,12 +20,26 @@ export interface Props extends $preact.Props {
  *
  * @returns       VNode / JSX element tree.
  */
-export default (props: Props = {}): $preact.VNode<Props> => {
-	return (
-		<Location url={props.url}>
-			<ErrorBoundary>
-				<Router>{props.children}</Router>
-			</ErrorBoundary>
-		</Location>
+const Component = (props: Props = {}): $preact.VNode<Props> => {
+	const RouterJSX = (
+		<ErrorBoundary>
+			<Router>{props.children}</Router>
+		</ErrorBoundary>
 	);
+	return $isꓺempty(useLocation()) ? <Location url={props.url}>{RouterJSX}</Location> : RouterJSX;
 };
+
+/**
+ * Exports the rest of our ISO APIs.
+ */
+export { Route, useLocation, useRoute, lazy, prerender, hydrate } from '../../apis/iso.js';
+
+/**
+ * Exports component by name.
+ */
+export { Component as Router };
+
+/**
+ * Default export.
+ */
+export default Component;
