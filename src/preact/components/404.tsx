e7d3ca7f12ca3746c692c63c90dbcfe914accdbb
Jason Caldwell <jaswrks@o5p.me>
2023-08-22 14:59:36 -0400

    Project update. [p][robotic]

---
+++ src/preact/components/404.tsx
@@ -0,0 +1,60 @@
+/**
+ * Preact component.
+ */
+
+import HTML from './html.js';
+import Head from './head.js';
+import Body from './body.js';
+import * as $preact from '../../preact.js';
+
+/**
+ * Defines types.
+ */
+export type Props = $preact.Props;
+
+/**
+ * Renders component.
+ *
+ * @param   props Component props.
+ *
+ * @returns       VNode / JSX element tree.
+ *
+ * @note This component should be easy to render as a string and then for it be easily
+ * dropped into any system, serving as a 404 error page; e.g., for a Cloudflare Pages site.
+ */
+export default (/* props: Props = {} */): $preact.VNode<Props> => {
+	return (
+		<HTML>
+			<Head
+				robots={'noindex, nofollow'}
+				title={'404 Error: Not Found'}
+				description={'The resource you are looking for could not be found.'}
+				stylePath={'https://cdn.clevercanyon.com/assets/uploads/404.css'}
+			/>
+			<Body>
+				<div class='noise' />
+				<div class='overlay' />
+				<div class='terminal'>
+					<h1>
+						{'404 Error: '}
+						<span class='error-message'>{'Not Found'}</span>
+					</h1>
+					<p class='output'>{'The resource you are looking for could not be found.'}</p>
+					<p class='output'>
+						{'Please '}
+						{/* eslint-disable */}
+						{/* @ts-ignore ... onClick intentionally stringified here. */}
+						<a href='#' onClick={'event.preventDefault(); history.back();'}>
+							{'go back'}
+						</a>
+						{/* eslint-enable */}
+						{' or '}
+						<a href='/'>{'return to the front page'}</a>
+						{'.'}
+					</p>
+					<p class='output'>{'Good luck.'}</p>
+				</div>
+			</Body>
+		</HTML>
+	);
+};
