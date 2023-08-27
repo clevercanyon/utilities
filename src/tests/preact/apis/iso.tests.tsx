64592ac6ceeae5f00150079c15896e5bd0aae456
Jason Caldwell <jaswrks@o5p.me>
2023-08-26 21:07:18 -0400

    Add preact tests.

---
+++ src/tests/preact/apis/iso.tests.tsx
@@ -0,0 +1,27 @@
+/**
+ * Test suite.
+ */
+
+import { $preact } from '../../../index.js';
+import { describe, test, expect } from 'vitest';
+import * as $preactꓺiso from '../../../preact/apis/iso.js';
+import { default as Router, RouterProps, Route, lazy } from '../../../preact/components/router.js';
+
+describe('$preactꓺiso tests', async () => {
+	test('$preactꓺiso.prerenderSPA()', async () => {
+		const markup = await $preactꓺiso.prerenderSPA({
+			request: new Request('https://localhost'),
+			appManifest: { 'index.html': { css: ['style.css'], file: 'script.js' } },
+			App: (props: RouterProps): $preact.VNode<RouterProps> => {
+				return (
+					<Router {...props}>
+						<Route default component={lazy(() => import('../../../preact/components/404.js'))} />
+					</Router>
+				);
+			},
+		});
+		console.log(markup);
+		expect(markup).toContain('<!DOCTYPE html>');
+		expect(markup).toContain('</html>');
+	});
+});
