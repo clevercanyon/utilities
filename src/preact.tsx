c74b1477e98762da2cdb0a4c490208e5b688d5fd
Jason Caldwell <jaswrks@o5p.me>
2023-10-01 06:00:24 -0400

    Project update. [p][robotic]

--- src/preact.tsx
+++ src/preact.tsx
@@ -65,11 +65,13 @@ export { lazyRoute, lazyComponent } from './preact/components/router.tsx';
 /**
  * Defines types.
  */
-export type { Dispatch } from 'preact/hooks';
-export type VNode<P extends Props = Props> = preact.VNode<P>;
 export type FnComponent<P extends Props = Props> = preact.FunctionComponent<P>;
 export type AsyncFnComponent<P extends Props = Props> = (...args: Parameters<FnComponent<P>>) => Promise<ReturnType<FnComponent<P>>>;
-export type Props<P extends object = $type.Object> = preact.RenderableProps<Readonly<P & { classes?: unknown }>>;
+
+export type VNode<P extends Props = Props> = preact.VNode<P>;
+export type Classes = TypesOfClasses | (TypesOfClasses | Classes)[];
+type TypesOfClasses = undefined | null | boolean | string | $type.Object<{ classes?: Classes }> | { [x: string]: boolean };
+export type Props<P extends object = $type.Object> = preact.RenderableProps<Readonly<P & { classes?: Classes }>>;
 
 export type { Props as DataProps } from './preact/components/data.tsx';
 export type { Props as HTMLProps } from './preact/components/html.tsx';
@@ -77,10 +79,12 @@ export type { Props as HeadProps } from './preact/components/head.tsx';
 export type { Props as BodyProps } from './preact/components/body.tsx';
 export type { Props as RouterProps } from './preact/components/router.tsx';
 
+export type { Dispatch } from 'preact/hooks';
+
 /**
  * Defines dirty props.
  */
-const dirtyProps = ['key', 'jsx', 'dangerouslySetInnerHTML', 'ref', 'children'];
+const dirtyProps = ['key', 'jsx', 'ref', 'children', 'dangerouslySetInnerHTML'];
 
 /**
  * Cleans VNode props.
@@ -99,7 +103,7 @@ export const cleanProps = <Type extends Props, Key extends keyof Type>(props: Ty
 /**
  * Formats component classes.
  *
- * @param   ...variadicArgs Classes.
+ * @param   ...variadicArgs {@see Classes[]}.
  *
  *   - Strings, however deep, are split on whitespace.
  *   - Arrays are traversed recursively, looking for strings.
@@ -114,29 +118,29 @@ export const cleanProps = <Type extends Props, Key extends keyof Type>(props: Ty
  * @see https://www.npmjs.com/package/clsx
  * @see https://www.npmjs.com/package/classnames
  */
-export const classes = (...variadicArgs: unknown[]): undefined | string => {
-    let list: unknown[] = []; // Initialize.
+export const classes = (...variadicClasses: Classes[]): undefined | string => {
+    let flat: unknown[] = []; // Initialize.
 
-    for (const args of variadicArgs) {
+    for (const args of variadicClasses) {
         for (const arg of $to.array(args)) {
             if ($is.string(arg)) {
-                list = list.concat(arg.split(/\s+/u));
+                flat = flat.concat(arg.split(/\s+/u));
                 //
             } else if ($is.array(arg) /* recursive */) {
-                list = list.concat((classes(arg) || '').split(/\s+/u));
+                flat = flat.concat((classes(arg) || '').split(/\s+/u));
                 //
             } else if ($is.object(arg)) {
                 if (Object.hasOwn(arg, 'classes')) {
-                    list = list.concat($to.array(arg.classes));
+                    flat = flat.concat($to.array(arg.classes));
                 } else {
                     for (const [classes, enable] of Object.entries(arg)) {
-                        if (true === enable) list = list.concat(classes.split(/\s+/u));
+                        if (true === enable) flat = flat.concat(classes.split(/\s+/u));
                     }
                 }
             }
         }
     } // We only want unique, non-empty, string CSS class names.
-    list = [...new Set(list.filter($is.string).filter($is.notEmpty))];
+    flat = [...new Set(flat.filter((c) => $is.string(c)).filter((c) => $is.notEmpty(c)))];
 
-    return !list.length ? undefined : list.join(' ');
+    return !flat.length ? undefined : flat.join(' ');
 };
