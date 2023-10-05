644a0121bc7cd9db2ad4cdc7a379d1968246886d
Jason Caldwell <jaswrks@o5p.me>
2023-10-04 23:13:54 -0400

    Project update. [p][robotic]

--- src/preact/components.tsx
+++ src/preact/components.tsx
@@ -2,7 +2,6 @@
  * All Preact components.
  */
 
-export { default as Aside, type Props as AsideProps } from './components/aside.tsx';
 export { default as Body, type Props as BodyProps } from './components/body.tsx';
 export { default as Data, type Props as DataProps } from './components/data.tsx';
 export { default as Error404, type Props as Error404Props } from './components/error-404.tsx';
@@ -10,7 +9,4 @@ export { default as Head, type Props as HeadProps } from './components/head.tsx'
 export { default as HTML, type Props as HTMLProps } from './components/html.tsx';
 export { default as LayoutContext, type Props as LayoutContextProps } from './components/layout-context.tsx';
 export { default as Link, type Props as LinkProps } from './components/link.tsx';
-export { default as Main, type Props as MainProps } from './components/main.tsx';
-export { default as Midsection, type Props as MidsectionProps } from './components/midsection.tsx';
 export { Route, default as Router, type LocationProps, type RouteContextAsProps, type RouteProps, type Props as RouterProps } from './components/router.tsx';
-export { default as Wrapper, type Props as WrapperProps } from './components/wrapper.tsx';
