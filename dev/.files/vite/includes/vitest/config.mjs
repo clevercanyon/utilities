d3effd2eb18f73954d2ed86621d91ba7613e32ea
Jason Caldwell <jaswrks@o5p.me>
2023-12-13 17:25:41 -0500

    Project update. [p][robotic]

--- dev/.files/vite/includes/vitest/config.mjs
+++ dev/.files/vite/includes/vitest/config.mjs
@@ -173,7 +173,8 @@ export default async ({ projDir, srcDir, logsDir, targetEnv, vitestSandboxEnable
         allowOnly: true, // Allows `describe.only`, `test.only`, `bench.only`.
 
         watch: false, // Disable watching by default; instead use `--watch`.
-        forceRerunTriggers: [],
+        forceRerunTriggers: [], // Disable; we’ll perform our own full re-runs when necessary.
+        // One of the reasons for disabling this is because it doesn’t support negated `!` patterns.
 
         reporters: ['verbose'], // Verbose reporting.
         // {@see https://o5p.me/p0f9j5} for further details.
