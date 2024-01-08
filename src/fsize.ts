9962b8442077cbffc64e78f970187a2800efbe97
Jason Caldwell <jaswrks@o5p.me>
2024-01-07 20:55:13 -0500

    Project update. [p][robotic]

--- src/fsize.ts
+++ src/fsize.ts
@@ -5,31 +5,31 @@
 import '#@initialize.ts';
 
 /**
- * Kilobytes, kibibytes.
+ * Kilobytes (kb), kibibytes (kB).
  */
 export const bytesInKilobyte = 1000;
 export const bytesInKibibyte = 1024;
 
 /**
- * Megabytes, mebibytes.
+ * Megabytes (MB), mebibytes (MiB).
  */
 export const bytesInMegabyte = bytesInKilobyte * 1000;
 export const bytesInMebibyte = bytesInKibibyte * 1024;
 
 /**
- * Gigabytes, gibibytes.
+ * Gigabytes (GB), gibibytes (GiB).
  */
 export const bytesInGigabyte = bytesInMegabyte * 1000;
 export const bytesInGibibyte = bytesInMebibyte * 1024;
 
 /**
- * Terabytes, tebibytes.
+ * Terabytes (TB), tebibytes (TiB).
  */
 export const bytesInTerabyte = bytesInGigabyte * 1000;
 export const bytesInTebibyte = bytesInGibibyte * 1024;
 
 /**
- * Petabytes, pebibytes.
+ * Petabytes (PB), pebibytes (PiB).
  */
 export const bytesInPetabyte = bytesInTerabyte * 1000;
 export const bytesInPebibyte = bytesInTebibyte * 1024;
