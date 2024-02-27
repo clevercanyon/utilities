8d20a306f8aa387a244cdc241ca26f8ef36adfdf
Jason Caldwell <jaswrks@o5p.me>
2024-02-27 12:10:47 -0500

    Project update. [p][robotic]

--- src/gzip.ts
+++ src/gzip.ts
@@ -4,19 +4,27 @@
 
 import '#@initialize.ts';
 
-import { $bytes, $str } from '#index.ts';
+import { $bytes, $obj, $str } from '#index.ts';
+
+/**
+ * Defines types.
+ */
+export type EncodeOptions = { deflate?: boolean };
+export type DecodeOptions = { deflate?: boolean };
 
 /**
  * Gzip-encodes a string into a compressed byte array.
  *
- * @param   str String to gzip.
+ * @param   str     String to gzip.
+ * @param   options All optional; {@see EncodeOptions}.
  *
- * @returns     Byte array promise; {@see Uint8Array}.
+ * @returns         Byte array promise; {@see Uint8Array}.
  */
-export const encode = async (str: string): Promise<Uint8Array> => {
-    const chunks = [],
+export const encode = async (str: string, options?: EncodeOptions): Promise<Uint8Array> => {
+    const opts = $obj.defaults({}, options || {}, { deflate: false }) as Required<EncodeOptions>,
+        chunks = [], // Initializes chunks.
         readableStream = new Blob([str]).stream(),
-        compressedStream = readableStream.pipeThrough(new CompressionStream('gzip'));
+        compressedStream = readableStream.pipeThrough(new CompressionStream(opts.deflate ? 'deflate' : 'gzip'));
 
     for await (const chunk of compressedStream as unknown as Iterable<Uint8Array>) {
         chunks.push(chunk); // Chunks of byte arrays.
@@ -27,14 +35,16 @@ export const encode = async (str: string): Promise<Uint8Array> => {
 /**
  * Decodes a compressed byte array.
  *
- * @param   bytes Compressed byte array.
+ * @param   bytes   Compressed byte array.
+ * @param   options All optional; {@see DecodeOptions}.
  *
- * @returns       Decode string promise.
+ * @returns         Decode string promise.
  */
-export const decode = async (bytes: Uint8Array): Promise<string> => {
-    const chunks = [],
+export const decode = async (bytes: Uint8Array, options?: DecodeOptions): Promise<string> => {
+    const opts = $obj.defaults({}, options || {}, { deflate: false }) as Required<DecodeOptions>,
+        chunks = [], // Initializes chunks.
         readableStream = new Blob([bytes]).stream(),
-        decompressedStream = readableStream.pipeThrough(new DecompressionStream('gzip'));
+        decompressedStream = readableStream.pipeThrough(new DecompressionStream(opts.deflate ? 'deflate' : 'gzip'));
 
     for await (const chunk of decompressedStream as unknown as Iterable<Uint8Array>) {
         chunks.push(chunk); // Chunks of byte arrays.
--- src/tests/gzip/index.any.ts
+++ src/tests/gzip/index.any.ts
@@ -6,17 +6,34 @@ import { $gzip } from '#index.ts';
 import { describe, expect, test } from 'vitest';
 
 describe('$gzip', async () => {
+    const bytesMap = {
+        gzip: {
+            'abcdefghijklmnopqrstuvwxyz0123456789': new Uint8Array([
+                31, 139, 8, 0, 0, 0, 0, 0, 0, 19, 75, 76, 74, 78, 73, 77, 75, 207, 200, 204, 202, 206, 201, 205, 203, 47, 40, 44, 42, 46, 41, 45, 43, 175, 168, 172, 50, 48, 52, 50,
+                54, 49, 53, 51, 183, 176, 4, 0, 123, 242, 198, 223, 36, 0, 0, 0,
+            ]),
+        },
+        deflate: {
+            'abcdefghijklmnopqrstuvwxyz0123456789': new Uint8Array([
+                120, 156, 75, 76, 74, 78, 73, 77, 75, 207, 200, 204, 202, 206, 201, 205, 203, 47, 40, 44, 42, 46, 41, 45, 43, 175, 168, 172, 50, 48, 52, 50, 54, 49, 53, 51, 183,
+                176, 4, 0, 10, 202, 13, 45,
+            ]),
+        },
+    };
     test('.encode()', async () => {
-        expect(await $gzip.encode('abcdefghijklmnopqrstuvwxyz0123456789')).toBeInstanceOf(Uint8Array);
+        for (const [str, bytes] of Object.entries(bytesMap.gzip)) {
+            expect(await $gzip.encode(str)).toStrictEqual(bytes);
+        }
+        for (const [str, bytes] of Object.entries(bytesMap.deflate)) {
+            expect(await $gzip.encode(str, { deflate: true })).toStrictEqual(bytes);
+        }
     });
     test('.decode()', async () => {
-        expect(
-            await $gzip.decode(
-                new Uint8Array([
-                    31, 139, 8, 0, 0, 0, 0, 0, 0, 19, 75, 76, 74, 78, 73, 77, 75, 207, 200, 204, 202, 206, 201, 205, 203, 47, 40, 44, 42, 46, 41, 45, 43, 175, 168, 172, 50, 48, 52,
-                    50, 54, 49, 53, 51, 183, 176, 4, 0, 123, 242, 198, 223, 36, 0, 0, 0,
-                ]),
-            ),
-        ).toBe('abcdefghijklmnopqrstuvwxyz0123456789');
+        for (const [str, bytes] of Object.entries(bytesMap.gzip)) {
+            expect(await $gzip.decode(bytes)).toBe(str);
+        }
+        for (const [str, bytes] of Object.entries(bytesMap.deflate)) {
+            expect(await $gzip.decode(bytes, { deflate: true })).toBe(str);
+        }
     });
 });
