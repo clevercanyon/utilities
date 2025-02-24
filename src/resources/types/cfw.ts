a468c660d90cbb698b82ee7eb8c350c0c8142d01
Jason Caldwell <jaswrks@o5p.me>
2025-02-24 08:42:31 -0500

    Project update. [p][robotic]

--- src/resources/types/cfw.ts
+++ src/resources/types/cfw.ts
@@ -7,5 +7,6 @@ import { type $type } from '#index.ts';
 export type * from '@cloudflare/workers-types/experimental';
 import type * as cfw from '@cloudflare/workers-types/experimental';
 
-export type Request<CfHostMetadata = unknown> = cfw.Request<CfHostMetadata> & $type.RequestC10nProps;
+export type Request<CfHostMetadata = unknown, Cf = cfw.CfProperties<CfHostMetadata>> = cfw.Request<CfHostMetadata, Cf> & $type.RequestC10nProps;
+export type RequestInfo<CfHostMetadata = unknown, Cf = cfw.CfProperties<CfHostMetadata>> = Request<CfHostMetadata, Cf> | cfw.URL | string;
 export type RequestInit<Cf = cfw.CfProperties> = cfw.RequestInit<Cf> & $type.RequestC10nProps;
